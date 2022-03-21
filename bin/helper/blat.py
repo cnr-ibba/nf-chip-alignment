02f13ba7673dc373236e0416fb014549a6a9dfc3
Paolo Cozzi <bunop@libero.it>
2022-03-21 14:49:26 +0100

    :recycle: process alignments with class objects

---
+++ bin/helper/blat.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 13:59:36 2022
+
+@author: Paolo Cozzi <bunop@libero.it>
+"""
+
+import re
+import logging
+
+from Bio.SearchIO._model.query import QueryResult
+from Bio.SeqRecord import SeqRecord
+
+from .utils import complement
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+manifest_pattern = re.compile(
+    ".* iln_snp: (.*), iln_pos: (.*), iln_strand: (.*)")
+
+
+def parse_description(description):
+    match = re.search(manifest_pattern, description)
+    iln_snp, iln_pos, iln_strand = match.groups()
+    return iln_snp, int(iln_pos), iln_strand
+
+
+def check_strand(hsp):
+    if hsp.hit_strand < 0:
+        raise NotImplementedError("Reversed hit strand not yet managed")
+
+    if hsp.query_strand > 0:
+        return "forward"
+
+    else:
+        return "reverse"
+
+
+def get_alt_allele(snp, ref, orient):
+    """Get ALT allele giving the ref and orientation"""
+
+    if orient == 'reverse':
+        snp = complement(snp)
+
+    genotype = snp.split("/")
+
+    ref_index = genotype.index(ref)
+
+    # the alt allele is the non reference allele
+    if ref_index == 0:
+        return genotype[1]
+    else:
+        return genotype[0]
+
+
+def get_illumina_forward(snp, orient):
+    """Get the illumina forward SNP. If the strand is forward, is the same of
+    received from illumina. If not, reverse the SNP"""
+
+    if orient == 'reverse':
+        snp = complement(snp)
+
+    return snp
+
+
+class BlatResult():
+    result = None
+    filtered = None
+    is_filtered = False
+    snp_id = None
+    iln_snp = None
+    iln_pos = None
+    iln_strand = None
+    probe_len = None
+
+    def __init__(self, result: QueryResult = None):
+        if result:
+            self.result = result
+            self.snp_id = result.id
+
+    def read_sequence_manifest(self, sequence: SeqRecord):
+        # collect SNP info
+        self.iln_snp, self.iln_pos, self.iln_strand = parse_description(
+            sequence.description)
+        self.probe_len = self.result.seq_len
+
+        logger.info(
+            f"Processing SNP: {self.snp_id}, iln_snp: {self.iln_snp}, "
+            f"iln_pos: {self.iln_pos}, iln_strand: {self.iln_strand} "
+            f"probe length {self.probe_len}")
+
+    def filter_results(self, lenth_pct=0.95, ident_pct=97):
+        # hit represents a single database hit
+        logger.debug(f"Got {len(self.result.hits)} hits for {self.result.id}")
+
+        # filter results by score (query aligned)
+        def filter_hsps(hsp):
+            if hsp.is_fragmented:
+                logger.debug(
+                    f"Filtering out {hsp.hit_id}:{hsp.hit_range_all}: "
+                    f"Found {len(hsp.fragments)} fragments"
+                )
+                return False
+
+            # score is a function of probe length
+            if (hsp.score < self.probe_len * lenth_pct or
+                    hsp.ident_pct < ident_pct):
+                logger.debug(
+                    f"Filtering out {hsp.hit_id}:{hsp.hit_range_all}: "
+                    f"Bad Score: {hsp.score} (ident_pct {hsp.ident_pct})"
+                )
+                return False
+
+            logger.info(
+                f"Keeping {hsp.hit_id}:{hsp.hit_range_all}: "
+                f"Score: {hsp.score} (ident_pct {hsp.ident_pct})"
+            )
+
+            return True
+
+        filtered = self.result.hsp_filter(filter_hsps)
+
+        logger.debug(f"Got {len(filtered.hits)} hits after filtering")
+
+        if len(filtered.hits) == 0 or len(filtered.hsps) == 0:
+            logger.warning(
+                f"All alignements have been filtered out for {self.result.id}")
+
+            # set filtered as empty list
+            self.filtered = []
+
+        elif len(filtered.hits) > 1 or len(filtered.hsps) > 1:
+            for hsp in filtered.hsps:
+                logger.warning(
+                    f"{hsp.hit_id}:{hsp.hit_range_all}: "
+                    f"Score: {hsp.score} (ident_pct {hsp.ident_pct})"
+                )
+            logger.warning(
+                f"Got more alignment than expected for {self.result.id}")
+
+            # set filtered as empty list
+            self.filtered = []
+
+        else:
+            # track filtered entries
+            self.filtered = filtered
+
+        self.is_filtered = True
+
+    def process_alignments(self, id2chromosome):
+        """Returns an output record and the processed alignment"""
+
+        self.lines, self.alignments = [], []
+
+        if self.is_filtered and not self.filtered:
+            # return an empty row
+            line = [
+                self.snp_id, 0, 0, None, self.iln_snp, None,
+                self.iln_strand, None, None, None]
+            self.lines = [line]
+
+        else:
+            for line, alignment in self.__process_hits(id2chromosome):
+                self.lines.append(line)
+                self.alignments.append(alignment)
+
+        return self.lines, self.alignments
+
+    def __process_hits(self, id2chromosome):
+        for i, hit in enumerate(self.filtered.hits):
+            logger.info(f"Processing hit {i}: {hit.id} for {self.snp_id}")
+
+            # attempt to determine chromosome name
+            chrom = id2chromosome[hit.id]
+
+            logger.info(f"Detected chromosome for {hit.id} is {chrom}")
+
+            # hsp represents region(s) of significant alignments between
+            # query and hit sequences
+            logger.info(f"Got {len(hit.hsps)} hsp for {hit.id}")
+
+            for j, hsp in enumerate(hit.hsps):
+                logger.info(
+                    f"Hsp {j}: has {len(hsp.fragments)} fragments. "
+                    f"Query range {hsp.query_range_all} "
+                    f"({hsp.query_strand_all}), "
+                    f"Hit range {hsp.hit_range_all} ({hsp.hit_strand_all}), "
+                    f"Score {hsp.score}, ident_pct {hsp.ident_pct}")
+
+                orient = check_strand(hsp)
+
+                logger.info(f"Orient is '{orient}'")
+
+                # the SNP position in the alignment, supposing no gap in
+                # query sequence (mind to the query strand)
+                if hsp.query_strand > 1:
+                    aln_pos = self.iln_pos - hsp.query_start - 1
+                else:
+                    aln_pos = hsp.query_end - self.iln_pos
+
+                # check that is letter is a N
+                if hsp.aln[0][aln_pos].upper() != 'N':
+                    raise Exception("Cannot find the SNP in the alignment")
+
+                ref_pos = hsp.hit_start + aln_pos
+
+                # this is 0-based index
+                ref_allele = hsp.aln[1][aln_pos].upper()
+
+                logger.info(
+                    f"Reference allele: {ref_allele} at "
+                    f"{hit.id}:{ref_pos+1}"
+                )
+
+                alt_allele = get_alt_allele(
+                    self.iln_snp, ref_allele, orient)
+
+                logger.info(
+                    f"Alternative allele: {alt_allele} at "
+                    f"{hit.id}:{ref_pos+1}"
+                )
+
+                # alleles a la NCBI
+                alleles = "/".join(sorted([ref_allele, alt_allele]))
+
+                # define a new data row. Mind to 1-based positions
+                line = [
+                    self.snp_id, chrom, ref_pos+1, alleles, self.iln_snp,
+                    get_illumina_forward(self.iln_snp, orient),
+                    self.iln_strand, orient, ref_allele, alt_allele]
+
+                # annotate alignment
+                alignment = hsp.aln
+                alignment[-1].id = "{0}:{1}-{2}".format(
+                    chrom,
+                    hsp.hit_range[0],
+                    hsp.hit_range[1])
+
+                yield line, alignment
--- bin/process_alignment.py
+++ bin/process_alignment.py
@@ -15,7 +15,8 @@ import Bio.SeqIO
 import Bio.SearchIO
 import Bio.AlignIO
 
-from helper.utils import complement, text_or_gzip_open
+from helper.blat import BlatResult
+from helper.utils import text_or_gzip_open
 
 logger = logging.getLogger(__name__)
 
@@ -32,53 +33,6 @@ parser.add_argument(
 parser.add_argument(
     "-o", "--output_csv", required=True, help="Output CSV file")
 args = parser.parse_args()
-
-pattern = re.compile(
-    ".* iln_snp: (.*), iln_pos: (.*), iln_strand: (.*)")
-
-
-def parse_description(description):
-    match = re.search(pattern, description)
-    iln_snp, iln_pos, iln_strand = match.groups()
-    return iln_snp, int(iln_pos), iln_strand
-
-
-def check_strand(hsp):
-    if hsp.hit_strand < 0:
-        raise NotImplementedError("Reversed hit strand not yet managed")
-
-    if hsp.query_strand > 0:
-        return "forward"
-
-    else:
-        return "reverse"
-
-
-def get_alt_allele(snp, ref, orient):
-    """Get ALT allele giving the ref and orientation"""
-
-    if orient == 'reverse':
-        snp = complement(snp)
-
-    genotype = snp.split("/")
-
-    ref_index = genotype.index(ref)
-
-    # the alt allele is the non reference allele
-    if ref_index == 0:
-        return genotype[1]
-    else:
-        return genotype[0]
-
-
-def get_illumina_forward(snp, orient):
-    """Get the illumina forward SNP. If the strand is forward, is the same of
-    received from illumina. If not, reverse the SNP"""
-
-    if orient == 'reverse':
-        snp = complement(snp)
-
-    return snp
 
 
 def parse_chromosome(sequence):
@@ -137,154 +91,28 @@ if __name__ == '__main__':
     writer.writerow(header)
 
     for result in Bio.SearchIO.parse(args.alignment, "blat-psl", pslx=True):
-        logger.info("-------------------------------------------------------")
         # result represent a single search query
+        logger.info("-------------------------------------------------------")
+        result = BlatResult(result)
 
         # collect SNP info
-        snp_sequence = chip_sequences[result.id]
-        iln_snp, iln_pos, iln_strand = parse_description(
-            snp_sequence.description)
+        snp_sequence = chip_sequences[result.snp_id]
+        result.read_sequence_manifest(snp_sequence)
 
-        logger.info(
-            f"Processing SNP: {result.id}, iln_snp: {iln_snp}, "
-            f"iln_pos: {iln_pos}, iln_strand: {iln_strand} "
-            f"probe length {result.seq_len}")
+        # filter alignments
+        result.filter_results()
 
-        # hit represents a single database hit
-        logger.debug(f"Got {len(result.hits)} hits for {result.id}")
+        # process SNP informations
+        lines, alignments = result.process_alignments(id2chromosome)
 
-        # filter results by score (query aligned)
-        def filter_hsps(hsp):
-            if hsp.is_fragmented:
-                logger.debug(
-                    f"Filtering out {hsp.hit_id}:{hsp.hit_range_all}: "
-                    f"Found {len(hsp.fragments)} fragments"
-                )
-                return False
-
-            if hsp.score < result.seq_len * 0.95 or hsp.ident_pct < 97:
-                logger.debug(
-                    f"Filtering out {hsp.hit_id}:{hsp.hit_range_all}: "
-                    f"Bad Score: {hsp.score} (ident_pct {hsp.ident_pct})"
-                )
-                return False
-
-            logger.info(
-                f"Keeping {hsp.hit_id}:{hsp.hit_range_all}: "
-                f"Score: {hsp.score} (ident_pct {hsp.ident_pct})"
-            )
-
-            return True
-
-        filtered = result.hsp_filter(filter_hsps)
-
-        logger.debug(f"Got {len(filtered.hits)} hits after filtering")
-
-        if len(filtered.hits) == 0 or len(filtered.hsps) == 0:
-            logger.warning(
-                f"All alignements have been filtered out for {result.id}")
-
-            # write an empty row
-            line = [
-                result.id, 0, 0, None, iln_snp, None, iln_strand, None,
-                None, None]
+        for line in lines:
             logger.debug(f"Writing {line}")
             writer.writerow(line)
 
-            # skip processing
-            continue
-
-        elif len(filtered.hits) > 1 or len(filtered.hsps) > 1:
-            for hsp in filtered.hsps:
-                logger.warning(
-                    f"{hsp.hit_id}:{hsp.hit_range_all}: "
-                    f"Score: {hsp.score} (ident_pct {hsp.ident_pct})"
-                )
-            logger.warning(
-                f"Got more alignment than expected for {result.id}")
-
-            # write an empty row
-            line = [
-                result.id, 0, 0, None, iln_snp, None, iln_strand, None,
-                None, None]
-            logger.debug(f"Writing {line}")
-            writer.writerow(line)
-
-            # skip processing
-            continue
-
-        for i, hit in enumerate(filtered.hits):
-            logger.info(f"Processing hit {i}: {hit.id} for {result.id}")
-
-            # attempt to determine chromosome name
-            chrom = id2chromosome[hit.id]
-
-            logger.info(f"Detected chromosome for {hit.id} is {chrom}")
-
-            # hsp represents region(s) of significant alignments between
-            # query and hit sequences
-            logger.info(f"Got {len(hit.hsps)} hsp for {hit.id}")
-
-            for j, hsp in enumerate(hit.hsps):
-                logger.info(
-                    f"Hsp {j}: has {len(hsp.fragments)} fragments. "
-                    f"Query range {hsp.query_range_all} "
-                    f"({hsp.query_strand_all}), "
-                    f"Hit range {hsp.hit_range_all} ({hsp.hit_strand_all}), "
-                    f"Score {hsp.score}, ident_pct {hsp.ident_pct}")
-
-                orient = check_strand(hsp)
-
-                logger.info(f"Orient is '{orient}'")
-
-                # the SNP position in the alignment, supposing no gap in
-                # query sequence (mind to the query strand)
-                if hsp.query_strand > 1:
-                    aln_pos = iln_pos - hsp.query_start - 1
-                else:
-                    aln_pos = hsp.query_end - iln_pos
-
-                # check that is letter is a N
-                if hsp.aln[0][aln_pos].upper() != 'N':
-                    raise Exception("Cannot find the SNP in the alignment")
-
-                ref_pos = hsp.hit_start + aln_pos
-
-                # this is 0-based index
-                ref_allele = hsp.aln[1][aln_pos].upper()
-
-                logger.info(
-                    f"Reference allele: {ref_allele} at "
-                    f"{hit.id}:{ref_pos+1}"
-                )
-
-                alt_allele = get_alt_allele(iln_snp, ref_allele, orient)
-
-                logger.info(
-                    f"Alternative allele: {alt_allele} at "
-                    f"{hit.id}:{ref_pos+1}"
-                )
-
-                # alleles a la NCBI
-                alleles = "/".join(sorted([ref_allele, alt_allele]))
-
-                # define a new data row. Mind to 1-based positions
-                line = [
-                    result.id, chrom, ref_pos+1, alleles, iln_snp,
-                    get_illumina_forward(iln_snp, orient), iln_strand, orient,
-                    ref_allele, alt_allele]
-                logger.debug(f"Writing {line}")
-                writer.writerow(line)
-
-                # annotate alignment
-                hsp.aln[-1].id = "{0}:{1}-{2}".format(
-                    chrom,
-                    hsp.hit_range[0],
-                    hsp.hit_range[1])
-
-                # write alignment
-                if args.output_aln:
-                    Bio.AlignIO.write([hsp.aln], output_aln_fh, "clustal")
+        # write alignments
+        if args.output_aln:
+            for aln in alignments:
+                Bio.AlignIO.write([aln], output_aln_fh, "clustal")
 
         # debug
         # break
